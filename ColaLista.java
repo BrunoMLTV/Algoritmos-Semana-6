3355435950d645a6a90459d0ead2194422d913c1
BrunoMLTV <bm665778@gmail.com>
2026-05-15 14:27:18 -0500

    Update ColaLista.java

--- ColaLista.java
+++ ColaLista.java
@@ -9,61 +9,43 @@ package lab06;
  * @author LAB-USR-AREQUIPA
  */
 public class ColaLista {
-
     private Nodo frente;
     private Nodo fin;
-
     // Enqueue
     public void enqueue(int dato) {
-
         Nodo nuevo = new Nodo(dato);
-
         if (fin == null) {
             frente = nuevo;
             fin = nuevo;
             return;
         }
-
         fin.siguiente = nuevo;
-
         fin = nuevo;
     }
-
     // Dequeue
     public int dequeue() {
-
         if (frente == null) {
             System.out.println("Cola vacía");
             return -1;
         }
-
         int dato = frente.dato;
-
         frente = frente.siguiente;
-
         if (frente == null) {
             fin = null;
         }
-
         return dato;
     }
-
     public void mostrar() {
 
         if (frente == null) {
             System.out.println("Cola vacía");
             return;
         }
-
         Nodo actual = frente;
-
         while (actual != null) {
-
             System.out.print(actual.dato + " ");
-
             actual = actual.siguiente;
         }
-
         System.out.println();
     }
 }
